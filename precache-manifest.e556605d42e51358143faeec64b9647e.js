76245111e8fd40bbf8efd65ab7a9ef731f17b69e
Eugene <eugene.veprytskyi@gmail.com>
2022-05-11 09:37:17 +0100

    new site

---
+++ precache-manifest.e556605d42e51358143faeec64b9647e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2035abf118171e12041d6883501d16c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ddd54f03f671823905",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "d06cd70226f897322b25",
+    "url": "/static/css/main.f203c762.chunk.css"
+  },
+  {
+    "revision": "c5ddd54f03f671823905",
+    "url": "/static/js/2.6c28458c.chunk.js"
+  },
+  {
+    "revision": "81e6e8d2593b98323b0ceb0508590d85",
+    "url": "/static/js/2.6c28458c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d06cd70226f897322b25",
+    "url": "/static/js/main.08166268.chunk.js"
+  },
+  {
+    "revision": "2ecf2e889cc3b8301492",
+    "url": "/static/js/runtime-main.64f83ec0.js"
+  },
+  {
+    "revision": "38ccc2053196a90155a764c365821a7e",
+    "url": "/static/media/Double Ring-1s-200px.38ccc205.svg"
+  },
+  {
+    "revision": "97c2c115880a1ccbe905987a09f7dc6d",
+    "url": "/static/media/UC-33756c4b-b5b1-49ce-b0e6-4902d5ad32ae.97c2c115.jpg"
+  },
+  {
+    "revision": "af72c8223c085319f86dea9d37d2e45d",
+    "url": "/static/media/arrow.af72c822.svg"
+  },
+  {
+    "revision": "668b5675f667cd83f96304078b61a8d7",
+    "url": "/static/media/bg.668b5675.svg"
+  },
+  {
+    "revision": "09354fe35ad45b58e466c8921d5a7aba",
+    "url": "/static/media/celsium.09354fe3.svg"
+  },
+  {
+    "revision": "7e984e4156641b52febec3c01567dc4f",
+    "url": "/static/media/day.7e984e41.svg"
+  },
+  {
+    "revision": "476167d0495d08363da957e410e4dff0",
+    "url": "/static/media/download.476167d0.svg"
+  },
+  {
+    "revision": "b2e2ad6d82d7c240e46c4c2525d8c3a4",
+    "url": "/static/media/facebook.b2e2ad6d.svg"
+  },
+  {
+    "revision": "88c76fe1317ca4aaad302433b081ae97",
+    "url": "/static/media/googleImage.88c76fe1.svg"
+  },
+  {
+    "revision": "ed3babcd6aa2fd3c124fe78ce9a33f04",
+    "url": "/static/media/googleNews.ed3babcd.svg"
+  },
+  {
+    "revision": "75f7f1de4e7b51c025ae73c17c865fdd",
+    "url": "/static/media/googleTranslate.75f7f1de.svg"
+  },
+  {
+    "revision": "45bac8ef533eaa9386a8c1cbe4d9f64c",
+    "url": "/static/media/inoreader.45bac8ef.svg"
+  },
+  {
+    "revision": "183f8c08ea0e86619dceafa6117e19e4",
+    "url": "/static/media/instagram.183f8c08.svg"
+  },
+  {
+    "revision": "32032a98f57ef2048d2878aa33fc046a",
+    "url": "/static/media/next.32032a98.svg"
+  },
+  {
+    "revision": "643cb786cebe798bcea4f7a1b1e45bf9",
+    "url": "/static/media/night.643cb786.svg"
+  },
+  {
+    "revision": "32303069cad10be46b1b146c65aa0708",
+    "url": "/static/media/remove.32303069.svg"
+  },
+  {
+    "revision": "daad728b20d2d5ae1b2a1e5c454ed6cd",
+    "url": "/static/media/signature.daad728b.svg"
+  },
+  {
+    "revision": "854c6416343717934741cdf401fb222f",
+    "url": "/static/media/twitter.854c6416.svg"
+  },
+  {
+    "revision": "a833c043970f7c4f46cb8fef9ff79ba9",
+    "url": "/static/media/youTube.a833c043.svg"
+  }
+]);
